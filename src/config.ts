1d55669392f490c5ebb4a4bd9b857dcb5187fc60
Harrison Ratcliffe <harrison.ratcliffe@me.com>
2023-06-14 12:05:07 +0100

    initial commit

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const MY_NAME = 'Harrison R';
-
-export const SITE_TITLE = 'Harrison R';
-export const SITE_DESCRIPTION = "I'm Harrison. Technical Support & Software Engineer";
